7c1b19199108b5d3783132554334b44aeac9835b
Mahmut Alhasan <Mahmoud963tr@gmail.com>
2026-01-12 22:13:31 +0300

    Update sample products - reset database with requested coffee varieties

--- sync_products.py
+++ sync_products.py
@@ -28,14 +28,16 @@ def create_sample_products():
         cursor.execute("DELETE FROM products")
         cursor.execute("DELETE FROM product_images")
         
-        # Örnek ürünler
+        # İstenen ürünler
         products = [
-            ("Brezilya Santos Koyu", "Koyu kavrum, bitter ve fındık notaları", "Koyu", 250.0, 480.0, 900.0, 50, "images/Brezilya_Santos_Koyu.png", "Brezilya", "Natur", 1),
-            ("Etiyopya Yirgacheffe", "Meyvemsi, çiçeksi ve çikolata notaları", "Orta", 280.0, 540.0, 1000.0, 30, "images/Etiyopya_Yirgacheffe_Ack.png", "Etiyopya", "Yıkanmış", 1),
-            ("Kolombiya Supremo", "Karamel ve kuru meyve aromaları", "Orta", 320.0, 600.0, 1100.0, 25, "images/Kolombiya_Supremo_Orta.png", "Kolombiya", "Yıkanmış", 1),
-            ("Guatemala Antigua", "Dumanlı, baharatlı ve çikolata notaları", "Koyu", 340.0, 650.0, 1200.0, 20, "images/Brezilya_Santos_Koyu.png", "Guatemala", "Yıkanmış", 1),
-            ("Kenya AA", "Asidik, vişne ve karabiber notaları", "Açık", 380.0, 720.0, 1300.0, 15, "images/Etiyopya_Yirgacheffe_Ack.png", "Kenya", "Yıkanmış", 0),
-            ("Sumatra Mandheling", "Toprak, mantar ve tropik meyve", "Koyu", 360.0, 680.0, 1250.0, 18, "images/Kolombiya_Supremo_Orta.png", "Endonezya", "Giling Basah", 1),
+            ("Guatemala Antigua", "Dumanlı, baharatlı ve çikolata notaları", "Orta", 340.0, 650.0, 1200.0, 25, "images/Guatemala_Antigua_Orta.png", "Guatemala", "Yıkanmış", 1),
+            ("Kenya AA", "Asidik, vişne ve karabiber notaları", "Açık", 380.0, 720.0, 1300.0, 20, "images/Kenya_AA_Ack.png", "Kenya", "Yıkanmış", 0),
+            ("Costa Rica Tarrazu", "Çikolata, narenciye ve karamel notaları", "Orta", 360.0, 680.0, 1250.0, 22, "images/Kolombiya_Supremo_Orta.png", "Kosta Rika", "Yıkanmış", 1),
+            ("Espresso Harmanı", "Espresso için özel harman, kremsi ve tatlı", "Koyu", 280.0, 520.0, 950.0, 40, "images/Brezilya_Santos_Koyu.png", "Harman", "Natur", 1),
+            ("Filtre Harmanı", "Filtre kahve için dengeli harman", "Orta", 260.0, 480.0, 880.0, 35, "images/Etiyopya_Yirgacheffe_Ack.png", "Harman", "Yıkanmış", 0),
+            ("Brezilya Santos", "Yoğun gövde, fındık ve çikolata", "Koyu", 250.0, 480.0, 900.0, 45, "images/Brezilya_Santos_Koyu.png", "Brezilya", "Natur", 1),
+            ("Etiyopya Yirgacheffe", "Meyvemsi, çiçeksi aromalar", "Orta", 280.0, 540.0, 1000.0, 30, "images/Etiyopya_Yirgacheffe_Ack.png", "Etiyopya", "Yıkanmış", 0),
+            ("Kolombiya Supremo", "Karamel ve kuru meyve", "Orta", 320.0, 600.0, 1100.0, 28, "images/Kolombiya_Supremo_Orta.png", "Kolombiya", "Yıkanmış", 1),
         ]
         
         cursor.executemany("""
@@ -44,7 +46,9 @@ def create_sample_products():
         """, products)
         
         conn.commit()
-        print(f"{len(products)} örnek ürün oluşturuldu")
+        print(f"{len(products)} ürün oluşturuldu:")
+        for product in products:
+            print(f"  - {product[0]} ({product[3]}₺/{product[4]}₺/{product[5]}₺)")
 
 if __name__ == "__main__":
     # Render'dan sync dene, olmazsa örnek ürünler oluştur
